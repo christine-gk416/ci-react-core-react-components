c8ad67140ef58fb63ec5d58fdf6273fde368e3b2
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-05-18 09:10:04 +0000

    Complete react components CI lesson

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -4,13 +4,19 @@ import StatefulGreeting from "./components/StatefulGreeting";
 import HooksCounter from "./components/HooksCounter";
 
 function App() {
-  return (
-    <div className="App">
-      <FunctionalGreeting name="John" />
-      {/* <StatefulGreeting name="Anna" /> */}
-      {/* <HooksCounter name="Mike" /> */}
-    </div>
-  );
+    return ( <
+        div className = "App" > {
+            // <
+            // FunctionalGreeting name = "John" / >
+        } {
+            <
+            StatefulGreeting name = "Bob" / >
+        } {
+            <
+            HooksCounter name = "Mike" / >
+        } <
+        /div>
+    );
 }
 
 export default App;
